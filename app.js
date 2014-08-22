04e99f7e42d6e6cb2719b83f81af22653cb8b7f3
zhongjian.zhou <zhongjian.zhou@dianpaing.com>
2014-08-22 10:45:38 +0800

    init

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var app = express();
+
+// app.all("*", function(req, res, next) {
+//     res.writeHead(200, {
+//         "Content-Type": "text/plain"
+//     });
+//     next();
+// });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.redirect('/index');
+});
+app.use('/users', require('./routes/users'));
+app.use('/index', require('./routes/index'));
+
+/// not match prev routes will catch 404, and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+http.createServer(app).listen(1337);
+
+module.exports = app;
